a67f833fe6190cbdeefc7bb1eed02c1be5fdf2ba
kennethillman <kenneth.illman@gmail.com>
2015-05-17 09:55:30 +0200

    Fresh start

--- tasks/clean.js
+++
@@ -1,7 +0,0 @@
-var del = require('del');
-
-module.exports = function (gulp, plugins, dirs) {
-	return function(cb) {
-		del([dirs.dev, dirs.dist], cb);
-	};
-};
--- tasks/deploy.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-
-module.exports = function (gulp, plugins, dirs, sftpOptions) {
-	return function () {
-		return gulp.src(path.join(dirs.dev, '**'))
-			.pipe(plugins.sftp(sftpOptions))
-	};
-};
